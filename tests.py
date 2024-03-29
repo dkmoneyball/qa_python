809f6490bf6669be812010247f6d5746ddf50103
Daniil <kutepov.aktau@gmail.com>
2024-03-29 12:19:24 +0300

    Revert "Merge pull request #2 from dkmoneyball/develop"
    This reverts commit 9c8b7ed9fcb17aa748ce225c773cacbed5188aed, reversing
    changes made to fcf75f2cb4618a1e2360fad44ad57f64fa1072ff.

--- tests.py
+++ tests.py
@@ -1,142 +1,24 @@
-import pytest
-
 from main import BooksCollector
 
-
+# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
+# обязательно указывать префикс Test
 class TestBooksCollector:
 
-
+    # пример теста:
+    # обязательно указывать префикс test_
+    # дальше идет название метода, который тестируем add_new_book_
+    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-
+        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-
+        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
+        # проверяем, что добавилось именно две
+        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
+        assert len(collector.get_books_rating()) == 2
 
-        assert len(collector.get_books_genre()) == 2
-
-    def test_add_new_book_add_one_books(self):
-
-        collector = BooksCollector()
-        collector.add_new_book('Путь война')
-
-        assert len(collector.get_books_genre()) == 1
-
-    def test_add_new_book_add_books_more_then_42_letters(self):
-
-        collector = BooksCollector()
-        collector.add_new_book('Путь война Путь война Путь война Путь война Путь война')
-
-        assert collector.books_genre == {}
-
-    def test_add_new_book_add_books_with_0_letters(self):
-
-        collector = BooksCollector()
-        collector.add_new_book('')
-
-        assert collector.books_genre == {}
-
-    def test_add_new_book_add_two_the_same_books(self):
-
-        collector = BooksCollector()
-
-        collector.add_new_book('Путь война')
-        collector.add_new_book('Путь война')
-
-        assert len(collector.get_books_genre()) == 1
-
-    def test_books_for_children_no_adult_genre(self):
-        collector = BooksCollector()
-
-        collector.add_new_book('Гарри Потер')
-        collector.add_new_book('Супер Ужасы')
-        collector.add_new_book('Супер Детектив')
-
-        collector.set_book_genre('Гарри Потер', 'Фантастика')
-        collector.set_book_genre('Супер Ужасы', 'Ужасы')
-        collector.set_book_genre('Супер Детектив', 'Детективы')
-
-        children_books = collector.get_books_for_children()
-
-        for book in children_books:
-            book_genre = collector.get_book_genre(book)
-            assert book_genre not in collector.genre_age_rating
-
-
-
-    def test_add_new_book_without_genre(self):
-
-        collector = BooksCollector()
-
-        collector.add_new_book('Некий тестовый том')
-        book_genre = collector.get_book_genre('Некий тестовый том')
-
-        assert book_genre == ''
-
-    def test_get_list_of_favorites_books(self):
-
-        collector = BooksCollector()
-
-
-        collector.add_new_book('Гарри Потер 1')
-        collector.add_new_book('Гарри Потер 2')
-        collector.add_new_book('Гарри Потер 3')
-        collector.add_book_in_favorites('Гарри Потер 1')
-        collector.add_book_in_favorites('Гарри Потер 3')
-
-
-        favorites = collector.get_list_of_favorites_books()
-
-
-        assert len(favorites) == 2
-        assert 'Гарри Потер 1' in favorites
-        assert 'Гарри Потер 2' not in favorites
-        assert 'Гарри Потер 3' in favorites
-
-    def test_delete_book_from_favorites(self):
-
-        collector = BooksCollector()
-
-
-        collector.add_new_book('Гарри Потер')
-        collector.add_book_in_favorites('Гарри Потер')
-        collector.delete_book_from_favorites('Гарри Потер')
-        favorites = collector.get_list_of_favorites_books()
-
-        assert 'Гарри Потер' not in favorites
-
-    def test_delete_non_existing_book_from_favorites(self):
-
-        collector = BooksCollector()
-
-
-        collector.add_new_book('Я дом семья')
-        collector.add_book_in_favorites('Я дом семья')
-        collector.delete_book_from_favorites('Я люблю бегать')
-        favorites = collector.get_list_of_favorites_books()
-
-        assert 'Я дом семья' in favorites
-
-
-    @pytest.mark.parametrize("books_in_favorites, book_to_delete, expected_favorites", [
-        (['Гарри Потер 1'], 'Гарри Потер 1', []),
-        (['Гарри Потер 1', 'Гарри Потер 2', 'Гарри Потер 3'], 'Гарри Потер 2', ['Гарри Потер 1', 'Гарри Потер 3']),
-        (['Гарри Потер 1', 'Гарри Потер 2', 'Гарри Потер 3'], 'Дары смерти', ['Гарри Потер 1', 'Гарри Потер 2', 'Гарри Потер 3']),
-    ])
-    def test_delete_book_from_favorites(self, books_in_favorites, book_to_delete, expected_favorites):
-
-        collector = BooksCollector()
-
-
-        for book in books_in_favorites:
-            collector.add_new_book(book)
-            collector.add_book_in_favorites(book)
-
-
-        collector.delete_book_from_favorites(book_to_delete)
-        favorites = collector.get_list_of_favorites_books()
-
-
-        assert favorites == expected_favorites
+    # напиши свои тесты ниже
+    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
